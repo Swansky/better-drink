a3912d1eecd811b2f7936389b5ff7adb8cf54f1f
Swansky <31344841+Swansky@users.noreply.github.com>
2024-10-16 20:34:31 +0200

    add String support for CommandExitMessage

--- src/main/java/com/jonahseguin/drink/exception/CommandExitMessage.java
+++ src/main/java/com/jonahseguin/drink/exception/CommandExitMessage.java
@@ -1,6 +1,7 @@
 package com.jonahseguin.drink.exception;
 
 import com.jonahseguin.drink.TranslateUtils;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TranslatableComponent;
 import org.bukkit.command.CommandSender;
 
@@ -8,10 +9,18 @@ import org.bukkit.command.CommandSender;
 public class CommandExitMessage extends Exception {
 
 
-    private final TranslatableComponent displayableMessage;
+    private final Component displayableMessage;
 
     public CommandExitMessage(TranslatableComponent message) {
         this.displayableMessage = message;
+    }
+
+    public CommandExitMessage(String message) {
+        this.displayableMessage = Component.text(message);
+    }
+
+    public CommandExitMessage(Component component) {
+        this.displayableMessage = component;
     }
 
     public void print(CommandSender sender) {
